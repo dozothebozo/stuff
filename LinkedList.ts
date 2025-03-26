bcbd76fc8017b28e6698127c0e8eca06669aabf4
dozo <dozothebozo@gmail.com>
2025-03-26 00:01:28 -0400

    fix: class Node

--- LinkedList.ts
+++ LinkedList.ts
@@ -1,6 +1,6 @@
 class Node<T> {
   value: T | null;
-  next: Node<T> | null;
+  next: Node<unknown> | null;
 
   constructor(value: T | null = null) {
     this.value = value;
@@ -9,9 +9,15 @@ class Node<T> {
 }
 
 const newNode = new Node<string>("hello world");
-const emptyNode = new Node()
+const emptyNode = new Node<string>(); 
+const numberNode = new Node<number>(1);
 
-newNode.next = emptyNode
+newNode.next = emptyNode;
+emptyNode.next = numberNode;
+
+console.log(newNode);
+console.log(emptyNode);
+
 
 console.log(newNode)
 console.log(emptyNode)
