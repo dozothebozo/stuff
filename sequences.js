e8e23fab6c83e676952c55631883b688df2ca3f0
dozothebozo <dozothebozo@gmail.com>
2026-02-02 17:07:49 -0500

    add(sequences): wip

---
+++ sequences.js
@@ -0,0 +1,31 @@
+// This program computes some sequences.
+
+function seq1() {
+  // 1 + 1/2 + 1/3 + 1/4 + ...
+
+}
+
+function seq2() {
+  // const + 2/3 + 3/5 + 4/7 + 5/9 + 6/11 + ...
+
+}
+
+function seq3() {
+  // e = 1 + 1/1! + 1/2! + 1/3! + 1/4! + ...
+}
+
+function seq4() {
+  // e^x = 1 + x/1! + x^2/2! + x^3/3! + 4^x/4! + ...
+
+}
+
+function seq5() {
+  // pie = 4(1 - 1/3 + 1/5 - 1/7 + 1/9 - ...)
+
+}
+
+seq1();
+// seq2();
+// seq3();
+// seq4();
+// seq5();
