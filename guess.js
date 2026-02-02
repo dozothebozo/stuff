f70aa8d05d08d84df40c14cf734461286e80e4fd
dozothebozo <dozothebozo@gmail.com>
2026-02-02 16:53:03 -0500

    add: newton's method for approximating square roots

---
+++ guess.js
@@ -0,0 +1,16 @@
+// This program approximates the square root of a number using Newton's method.
+
+function NewtonSqrt(x, guess, iterations) {
+  for (let i = 0; i < iterations; i++) {
+    guess = (guess + (x / guess)) / 2;
+  }
+  return guess;
+}
+
+let x = Number(prompt("Enter the number you want the square root of:"));
+let iterations = Number(prompt("Enter the number of iterations:"));
+let guess = x / 2;
+
+let finalGuess = NewtonSqrt(x, guess, iterations);
+
+console.log(`The approximate square root of ${x} is ${finalGuess}`);
