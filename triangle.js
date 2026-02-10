c5bc0a33824663bd0d0074c0b38b37a011578571
dozothebozo <dozothebozo@gmail.com>
2026-02-10 17:28:50 -0500

    add(triangle): wip

---
+++ triangle.js
@@ -0,0 +1,23 @@
+// Write a program that prints a triangle of digits, as shown in the examples below.
+//
+// Example 1:
+// Computer: How many rows?
+// User: 4
+// Computer:
+//
+// 1 2 3 4
+//   1 2 3
+//     1 2
+//       1
+//
+// Example 2:
+// Computer: How many rows?
+// User: 6
+// Computer:
+//
+// 1 2 3 4 5 6
+//   1 2 3 4 5
+//     1 2 3 4
+//       1 2 3
+//         1 2
+//           1
