3cf67c50205a54e6eef2dd007fbc81889896a7d2
dozothebozo <dozothebozo@gmail.com>
2026-02-13 11:15:24 -0500

    add(Ch2/p4): wip

--- BigCpp/Ch2/p4.cpp
+++ BigCpp/Ch2/p4.cpp
@@ -1,2 +1,21 @@
 // Write a program that prompts the user for a measurement in meters and then
 // converts it to miles, feet, and inches.
+
+#include <iostream>
+
+double getMeasurement() {
+  double x;
+  std::cout << "Enter a measurement (in meters): ";
+  std::cin >> x;
+  return x;
+}
+
+double miles(double measurement);
+double feet(double measurement);
+double inches(double measurement);
+
+int main() {
+  double measurement{getMeasurement()};
+  std::cout << measurement;
+  return 0;
+}
