29b110a5cc1255542ccf6991fe1ad5a4fb0765be
dozothebozo <dozothebozo@gmail.com>
2026-02-13 22:29:55 -0500

    add(Ch2/p5): solution

---
+++ BigCpp/Ch2/constants.h
@@ -0,0 +1,8 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+namespace constants {
+  constexpr double pi{3.14159};
+}
+
+#endif // !CONSTANTS_H
--- BigCpp/Ch2/p5.cpp
+++ BigCpp/Ch2/p5.cpp
@@ -1,3 +1,42 @@
-// Write a program that prompts the user for a radius and then prints
+// Write a program that prompts the user for a radius and then prints:
 // The area and circumference of a circle with that radius
 // The volume and surface area of a sphere with that radius
+
+#include <cmath>
+#include <iostream>
+#include "constants.h"
+
+double getRadius() {
+  double r{};
+  std::cout << "Enter the radius of a circle: ";
+  std::cin >> r;
+  return r;
+}
+
+double area(double r) {
+  return constants::pi * std::pow(r, 2);
+}
+
+double circumference(double r) {
+  return 2 * constants::pi * r;
+}
+
+double volume(double r) {
+  return 4.00 / 3.00 * constants::pi * std::pow(r, 3);
+}
+
+double surfaceArea(double r) {
+  return 4 * constants::pi * std::pow(r, 2);
+}
+
+int main() {
+  double radius{getRadius()};
+
+  std::cout << "Radius: " << radius << '\n';
+  std::cout << "Area: " << area(radius) << '\n';
+  std::cout << "Circumference: " << circumference(radius) << '\n';
+  std::cout << "Volume: " << volume(radius) << '\n';
+  std::cout << "Surface Area: " << surfaceArea(radius) << '\n';
+
+  return 0;
+}
