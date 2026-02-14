316d761e309fcbf69847ad46e241e0f049bf85e7
dozothebozo <dozothebozo@gmail.com>
2026-02-13 22:37:50 -0500

    add(Ch2/p2): solution

--- BigCpp/Ch2/p2.cpp
+++ BigCpp/Ch2/p2.cpp
@@ -1,2 +1,24 @@
 // Write a program that displays the squares, cubes, and fourth powers
 // of the numbers 1 through 5.
+
+#include <cmath>
+#include <iostream>
+
+void printPower(int x) {
+  std::cout << x << " squared: " << std::pow(x, 2) << '\n';
+  std::cout << x << " cubed: " << std::pow(x, 3) << '\n';
+  std::cout << x << " to the fourth power: " << std::pow(x, 4) << '\n';
+}
+
+int main() {
+  printPower(1);
+  std::cout << "--------------\n";
+  printPower(2);
+  std::cout << "--------------\n";
+  printPower(3);
+  std::cout << "--------------\n";
+  printPower(4);
+  std::cout << "--------------\n";
+  printPower(5);
+  return 0;
+}
