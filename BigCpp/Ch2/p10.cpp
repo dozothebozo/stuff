f3177c16b3eb3d405057a53519c710fd3773909f
dozothebozo <dozothebozo@gmail.com>
2026-02-13 22:57:37 -0500

    add(Ch2/p10): solution

--- BigCpp/Ch2/p10.cpp
+++ BigCpp/Ch2/p10.cpp
@@ -4,3 +4,38 @@
 // The price of gas per gallon
 // Then print out how far the car can go with the gas in the tank and print the
 // cost per 100 miles.
+
+#include <iostream>
+
+double getGallons() {
+  double gallons{};
+  std::cout << "Enter the number of gallons of gas in the tank: ";
+  std::cin >> gallons;
+  return gallons;
+}
+
+double getFuelEfficiency() {
+  double efficiency{};
+  std::cout << "Enter the fuel efficiency in miles per gallon: ";
+  std::cin >> efficiency;
+  return efficiency;
+}
+
+double getPrice() {
+  double price{};
+  std::cout << "Enter the price of gas per gallon: ";
+  std::cin >> price;
+  return price;
+}
+
+int main() {
+  double gallons{getGallons()};
+  double fuelEfficiency{getFuelEfficiency()};
+  double pricePerGallon{getPrice()};
+  double miles{gallons * fuelEfficiency};
+
+  std::cout << "With " << gallons << " gallons in the tank, you can go " << miles << " miles." << '\n';
+  std::cout << "The cost per 100 miles is: " << (100 / fuelEfficiency * pricePerGallon) << '\n';
+
+  return 0;
+}
