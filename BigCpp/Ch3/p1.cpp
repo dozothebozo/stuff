72f2ea1a7f1873ecb8cc040d0d14ab6c6330f621
dozothebozo <dozothebozo@gmail.com>
2026-02-19 13:05:31 -0500

    add(Ch3/p1): solution

--- BigCpp/Ch3/p1.cpp
+++ BigCpp/Ch3/p1.cpp
@@ -1,3 +1,26 @@
 // Write a program that prints all solutions to the quadratic equation: ax^2 + bx + c = 0
 // Read in a, b, c and use the quadratic formula. If the discriminant
 // is negative, display a message stating that there are no solutions.
+
+#include <cmath>
+#include <iostream>
+
+int main() {
+  int a{}, b{}, c{};
+  std::cout << "Enter terms (a b c): ";
+  std::cin >> a >> b >> c;
+
+  double discriminant{std::pow(b, 2) - 4 * a * c};
+  std::cout << "discriminant: " << discriminant << '\n';
+
+  if (discriminant < 0) {
+    std::cout << "no real solutions\n";
+  } else if (discriminant == 0) {
+    std::cout << "x = " << (-b / 2 * a) << '\n';
+  } else {
+    std::cout << "x1 = " << (-b + std::sqrt(discriminant)) / 2 * a << '\n';
+    std::cout << "x2 = " << (-b - std::sqrt(discriminant)) / 2 * a << '\n';
+  }
+
+  return 0;
+}
