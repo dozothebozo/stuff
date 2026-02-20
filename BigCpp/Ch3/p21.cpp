5ec017ebe99ba7737433f57c63b6a854bd88f867
dozothebozo <dozothebozo@gmail.com>
2026-02-20 17:42:06 -0500

    add(Ch3/p21): solution

--- BigCpp/Ch3/p21.cpp
+++ BigCpp/Ch3/p21.cpp
@@ -24,3 +24,44 @@
 // 17
 // 19
 //
+
+#include <cmath>
+#include <iostream>
+
+int getInteger() {
+  int n{};
+  std::cout << "Enter an integer: ";
+  std::cin >> n;
+  return n;
+}
+
+bool isPrime(int n) {
+  if (n <= 1)
+    return false;
+
+  if (n == 2)
+    return true;
+
+  if (n % 2 == 0)
+    return false;
+
+  for (int i{3}; i <= std::sqrt(n); i += 2) {
+    if (n % i == 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+int main() {
+  int n{getInteger()};
+
+  for (int i{2}; i <= n; ++i) {
+    if (isPrime(i)) {
+      std::cout << i << '\n';
+    }
+  }
+
+  return 0;
+}
