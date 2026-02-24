05010782563e4956e6c3a1a7f72c37602675d368
dozothebozo <dozothebozo@gmail.com>
2026-02-23 22:02:34 -0500

    add(Ch3/p12): solution

--- BigCpp/Ch3/p12.cpp
+++ BigCpp/Ch3/p12.cpp
@@ -21,3 +21,42 @@
 // Computer: Enter a year:
 // User: 1900
 // Computer: 1900 is not a leap year.
+
+#include <iostream>
+#include <limits>
+
+int getYear() {
+  int x{};
+
+  while (true) {
+    std::cout << "Enter a year: ";
+    if (std::cin >> x) {
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      return x;
+    } else {
+      std::cout << "Invalid input. Please try again.\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+}
+
+int main() {
+  char choice{};
+
+  do {
+    int year{getYear()};
+
+    if ((year < 1582 && year % 4 == 0) || (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))) {
+      std::cout << year << " is a leap year.\n";
+    } else {
+      std::cout << year << " is not a leap year.\n";
+    }
+
+    std::cout << "Do you want to continue? (y/n): ";
+    std::cin >> choice;
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+  } while (choice != 'n' && choice != 'N');
+
+  return 0;
+}
