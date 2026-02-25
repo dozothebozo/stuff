534f4d58fa3985f1e761392ad2531bba1ac70d25
dozothebozo <dozothebozo@gmail.com>
2026-02-25 18:27:13 -0500

    add(Ch3/p18): solution

--- BigCpp/Ch3/p18.cpp
+++ BigCpp/Ch3/p18.cpp
@@ -14,3 +14,45 @@
 // Computer: Enter the index n to compute the n-th Fibonacci number:
 // User: 6
 // Computer: Fibonacci number #6 is 8
+
+#include <iostream>
+#include <limits>
+
+int getIndex() {
+  int x{};
+
+  while (true) {
+    std::cout << "Enter the index n to compute the n-th Fibonacci number: ";
+    if (std::cin >> x) {
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      return x;
+    } else {
+      std::cout << "Invalid input. Please try again.\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+}
+
+int main() {
+  int n{getIndex()};
+
+  long long newer{1};
+  long long older{1};
+  long long current{};
+
+  if (n == 1 || n == 2) {
+    std::cout << "Fibonacci number #" << n << " is " << 1;
+    return 0;
+  }
+
+  for (int i{3}; i <= n; ++i) {
+    current = newer + older;
+    older = newer;
+    newer = current;
+  }
+
+  std::cout << "Fibonacci number #" << n << " is " << current;
+
+  return 0;
+}
