aa6484d73297ceaa568e67bdc09ef111f14d0aa4
dozothebozo <dozothebozo@gmail.com>
2026-02-26 15:55:22 -0500

    add(Ch3/p4): solution

--- BigCpp/Ch3/p4.cpp
+++ BigCpp/Ch3/p4.cpp
@@ -19,3 +19,54 @@
 // Computer: Enter a letter grade:
 // User: B-
 // Computer: The numeric value is 2.7.
+
+#include <iostream>
+#include <string>
+
+std::string getLetterGrade() {
+  std::string s{};
+  std::cout << "Enter a letter grade: ";
+  std::getline(std::cin >> std::ws, s);
+  return s;
+}
+
+int main() {
+  std::string letterGrade{getLetterGrade()};
+  double numericValue{};
+
+  switch (letterGrade[0]) {
+    case ('A'):
+      numericValue = 4.0;
+      break;
+    case ('B'):
+      numericValue = 3.0;
+      break;
+    case ('C'):
+      numericValue = 2.0;
+      break;
+    case ('D'):
+      numericValue = 1.0;
+      break;
+    case ('F'):
+      numericValue = 0.0;
+      break;
+    default:
+      std::cout << "Invalid letter grade.\n";
+      return 1;
+  }
+
+  if (letterGrade.length() <= 1 || letterGrade[0] == 'F') {
+    std::cout << "The numeric value is " << numericValue << ".\n";
+    return 0;
+  }
+
+  if (letterGrade[1] == '+' && letterGrade[0] != 'A') {
+    numericValue += 0.3;
+  } else if (letterGrade[1] == '-') {
+    numericValue -= 0.3;
+  }
+
+  std::cout << "The numeric value is " << numericValue << ".\n";
+
+  return 0;
+}
