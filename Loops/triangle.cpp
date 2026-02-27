2fc583698a23de467c275d5e2b6d4a22d6d73a6e
dozothebozo <dozothebozo@gmail.com>
2026-02-27 18:36:08 -0500

    add(Loops/triangle): solution

--- Loops/triangle.cpp
+++ Loops/triangle.cpp
@@ -36,3 +36,112 @@
 //  ***
 //   **
 //    *
+
+#include <iostream>
+#include <limits>
+
+// forward declarations
+int getRows();
+void printLeftAlignedTriangle(int rows);
+void printLeftAlignedInvertedTriangle(int rows);
+void printRightAlignedTriangle(int rows);
+void printRightAlignedInvertedTriangle(int rows);
+
+int getRows() {
+  int x{};
+
+  while (true) {
+    std::cout << "Enter the size of the triangle (number of rows): ";
+    if (std::cin >> x) {
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      return x;
+    } else {
+      std::cout << "Invalid input. Please try again.\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+}
+
+// Left-aligned standard triangle (stars increase from 1 to N).
+// *
+// **
+// ***
+// ****
+void printLeftAlignedTriangle(int rows) {
+  for (int i{0}; i < rows; ++i) {
+    for (int j{0}; j < i + 1; ++j) {
+      std::cout << "*";
+    }
+    std::cout << '\n';
+  }
+}
+
+// Left-aligned inverted triangle (stars decrease from N to 1).
+// ****
+// ***
+// **
+// *
+void printLeftAlignedInvertedTriangle(int rows) {
+  for (int i{0}; i < rows; ++i) {
+    for (int j{rows - i}; j >= 1; --j) {
+      std::cout << '*';
+    }
+    std::cout << '\n';
+  }
+}
+
+// Right-aligned standard triangle (requires a loop for spaces before the stars).
+//    *
+//   **
+//  ***
+// ****
+void printRightAlignedTriangle(int rows) {
+  for (int i{1}; i <= rows; ++i) {
+    // Print exactly (totalRows - i) spaces
+    for (int j{1}; j <= rows - i; ++j) {
+      std::cout << ' ';
+    }
+
+    // Print exactly 'i' stars
+    for (int k{1}; k <= i; ++k) {
+      std::cout << '*';
+    }
+    std::cout << '\n';
+  }
+}
+
+// Right-aligned inverted triangle.
+// ****
+//  ***
+//   **
+//    *
+void printRightAlignedInvertedTriangle(int rows) {
+  for (int i{rows}; i >= 1; --i) {
+    for (int j{1}; j <= rows - i; ++j) {
+      std::cout << ' ';
+    }
+    for (int k{1}; k <= i; k++) {
+      std::cout << '*';
+    }
+    std::cout << '\n';
+  }
+}
+
+int main() {
+  int rows{getRows()};
+
+  std::cout << "Left-aligned Triangle: \n\n";
+  printLeftAlignedTriangle(rows);
+
+  std::cout << "\nLeft-aligned Inverted Triangle: \n\n";
+  printLeftAlignedInvertedTriangle(rows);
+
+  std::cout << "\nRight-aligned Triangle: \n\n";
+  printRightAlignedTriangle(rows);
+
+  std::cout << "\nRight-aligned Inverted Triangle: \n\n";
+  printRightAlignedInvertedTriangle(rows);
+
+  return 0;
+}
