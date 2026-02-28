2c96c7ff147fef9cb998dd12068b1b2cd4117bbd
dozothebozo <dozothebozo@gmail.com>
2026-02-27 23:03:58 -0500

    add(Loops/times_table): solution

--- Loops/times_table.cpp
+++ Loops/times_table.cpp
@@ -21,3 +21,43 @@
 //   3  3  6  9 12 15
 //   4  4  8 12 16 20
 //   5  5 10 15 20 25
+
+#include <iomanip>
+#include <iostream>
+#include <limits>
+
+int getInteger() {
+  int x{};
+
+  while (true) {
+    std::cout << "Enter an integer: ";
+    if (std::cin >> x) {
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      return x;
+    } else {
+      std::cout << "Invalid input. Please try again.\n";
+      std::cin.clear();
+      std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    }
+  }
+}
+
+int main() {
+  int square{getInteger()};
+
+  std::cout << std::setw(1) << " ";
+  for (int i{1}; i <= square; ++i) {
+    std::cout << std::setw(3) << i;
+  }
+  std::cout << '\n';
+
+  for (int i{1}; i <= square; ++i) {
+    std::cout << i;
+    for (int j{1}; j <= square; ++j) {
+      std::cout << std::setw(3) << j * i;
+    }
+    std::cout << '\n';
+  }
+
+  return 0;
+}
