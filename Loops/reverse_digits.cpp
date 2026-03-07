09a90dcd8eccc143c0348bf34d8cd6c9e4c6460d
dozothebozo <dozothebozo@gmail.com>
2026-03-07 01:58:20 -0500

    add(Loops/reverse_digits): wip

--- Loops/reverse_digits.cpp
+++ Loops/reverse_digits.cpp
@@ -14,3 +14,47 @@
 // Computer: Enter an integer:
 // User: 7349
 // Computer: Reversed integer: 9437
+
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+
+void ignoreLine() {
+  std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+bool clearFailedExtraction() {
+  if (!std::cin) {
+    if (std::cin.eof()) {
+      std::exit(0);
+    }
+    std::cin.clear();
+    ignoreLine();
+    return true;
+  }
+  return false;
+}
+
+int getInteger() {
+  while (true) {
+    int x{};
+    std::cout << "Enter an integer: ";
+    std::cin >> x;
+
+    if (clearFailedExtraction()) {
+      std::cout << "Invalid input. Try again.\n";
+      continue;
+    }
+
+    ignoreLine();
+
+    return x;
+  }
+}
+
+int main() {
+  int num{getInteger()};
+  std::cout << num;
+
+  return 0;
+}
