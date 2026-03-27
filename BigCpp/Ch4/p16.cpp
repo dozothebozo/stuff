a5cf942dcd6202847e6bb439801357a597ffd0b4
dozothebozo <dozothebozo@gmail.com>
2026-03-27 15:42:47 -0400

    add(Ch4/p16): solution

--- BigCpp/Ch4/p16.cpp
+++ BigCpp/Ch4/p16.cpp
@@ -36,3 +36,144 @@
 //
 // Example Output:
 // |:|::|:::||::|:|::|::|:|:|::|||
+
+#include <cstdlib>
+#include <iostream>
+#include <limits>
+#include <string>
+
+void ignoreLine();
+bool recoverStream();
+std::string getString(const std::string& prompt);
+bool isFiveDigitZipcode(const std::string& s);
+int getCheckDigit(const std::string& zipcode);
+std::string digitEncoding(int digit);
+std::string getScheme(const std::string& code);
+
+int main()
+{
+    std::string zipcode{getString("Enter a 5 digit zipcode: ")};
+    int checkDigit{getCheckDigit(zipcode)};
+    std::string scheme{getScheme(zipcode + std::to_string(checkDigit))};
+
+    std::cout << scheme << '\n';
+
+    return 0;
+}
+
+void ignoreLine()
+{
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+bool recoverStream()
+{
+    if (!std::cin)
+    {
+        if (std::cin.eof())
+        {
+            exit(1);
+        }
+
+        std::cin.clear();
+        ignoreLine();
+        return true;
+    }
+
+    return false;
+}
+
+bool isFiveDigitZipcode(const std::string& s)
+{
+    if (s.length() != 5)
+    {
+        return false;
+    }
+
+    for (unsigned int i{0}; i < s.length(); ++i)
+    {
+        if (s[i] < '0' || s[i] > '9')
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+std::string getString(const std::string& prompt)
+{
+    while (true)
+    {
+        std::string s{};
+        std::cout << prompt;
+        std::cin >> s;
+
+        if (recoverStream() || !isFiveDigitZipcode(s))
+        {
+            std::cout << "Invalid input. Try again.\n";
+            continue;
+        }
+
+        return s;
+    }
+}
+
+int getCheckDigit(const std::string& zipcode)
+{
+    int sum{};
+
+    for (unsigned int i{0}; i < zipcode.length(); ++i)
+    {
+        sum += zipcode[i] - '0';
+    }
+
+    if (sum % 10 == 0)
+    {
+        return 0;
+    }
+
+    return 10 - (sum % 10);
+}
+
+std::string digitEncoding(int digit)
+{
+    switch (digit)
+    {
+    case 0:
+        return "||:::";
+    case 1:
+        return ":::||";
+    case 2:
+        return "::|:|";
+    case 3:
+        return "::||:";
+    case 4:
+        return ":|::|";
+    case 5:
+        return ":|:|:";
+    case 6:
+        return ":||::";
+    case 7:
+        return "|:::|";
+    case 8:
+        return "|::|:";
+    case 9:
+        return "|:|::";
+    default:
+        return "";
+    }
+}
+
+std::string getScheme(const std::string& code)
+{
+    std::string s{"|"};
+
+    for (unsigned int i{0}; i < code.length(); ++i)
+    {
+        s += digitEncoding(code[i] - '0');
+    }
+
+    s += "|";
+    return s;
+}
