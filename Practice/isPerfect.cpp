3b90eb403c1f479ec2c36ae1ecfc9a728dd4511c
dozothebozo <dozothebozo@gmail.com>
2026-03-30 12:25:17 -0400

    add(Practice/isPerfect): solution

--- Practice/isPerfect.cpp
+++ Practice/isPerfect.cpp
@@ -4,5 +4,69 @@
 
 // Use this function in another function that will find all perfect numbers in an interval from the
 // first to the second argument of this function. Print each perfect number together with its
-// divisor sum. Print three results per line, in a well aligned table. Submit both functions and
-// your main program.
+// divisor sum. Print three results per line, in a well aligned table.
+
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+// function prototypes
+bool isPerfect(int num);
+void printPerfectNumberInRange(int min, int max);
+
+// global declarations of constants
+const int INTERVAL_MIN = 1;
+const int INTERVAL_MAX = 10000;
+
+int main()
+{
+    printPerfectNumberInRange(INTERVAL_MIN, INTERVAL_MAX);
+    return 0;
+}
+
+bool isPerfect(int num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+
+    int perfect{1};
+
+    for (int i{2}; i <= num / 2; ++i)
+    {
+        if (num % i == 0)
+        {
+            perfect += i;
+        }
+    }
+
+    return perfect == num;
+}
+
+void printPerfectNumberInRange(int min, int max)
+{
+    int count{};
+
+    for (int i{min}; i <= max; ++i)
+    {
+        if (isPerfect(i))
+        {
+            cout << i << "(sum = " << i << ")" << setw(10);
+
+            ++count;
+
+            if (count == 3)
+            {
+                cout << '\n';
+                count = 0;
+            }
+        }
+    }
+
+    if (count != 0)
+    {
+        cout << '\n';
+    }
+}
