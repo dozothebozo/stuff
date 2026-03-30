d69c3404969a9d16d0b17d25ac0974f07076b166
dozothebozo <dozothebozo@gmail.com>
2026-03-29 23:06:41 -0400

    add(Loops/basketball): solution

--- Loops/basketball.cpp
+++ Loops/basketball.cpp
@@ -20,3 +20,98 @@
 // Computer: Enter scores for Player 2:
 // User: 10 12 15 8 11
 // Computer: Player 2 Average: 11.2. High scoring games: 0
+
+#include <cstdlib>
+#include <iomanip>
+#include <iostream>
+#include <limits>
+#include <string>
+
+void ignoreLine();
+bool recoverStream();
+int getInteger(const std::string& prompt);
+
+const int GAMES{5};
+const int HIGH_SCORE_MIN{20};
+
+int main()
+{
+    int players{getInteger("How many players to analyze? ")};
+
+    while (players == 0)
+    {
+        std::cout << "Enter at least 1 player.\n";
+        players = getInteger("How many players to analyze? ");
+    }
+
+    std::cout << std::fixed << std::setprecision(1);
+
+    for (int i{1}; i <= players; ++i)
+    {
+        double totalPoints{};
+        int highScoringGames{};
+
+        std::cout << "Enter scores for Player " << i << '\n';
+
+        for (int j{1}; j <= GAMES; ++j)
+        {
+            std::string prompt{"score (" + std::to_string(j) + "/" + std::to_string(GAMES) + "): "};
+
+            int score{getInteger(prompt)};
+
+            if (score >= HIGH_SCORE_MIN)
+            {
+                ++highScoringGames;
+            }
+
+            totalPoints += score;
+        }
+
+        double average{totalPoints / GAMES};
+
+        std::cout << "Player " << i << " Average: " << average
+                  << ". High scoring games: " << highScoringGames << '\n';
+    }
+
+    return 0;
+}
+
+void ignoreLine()
+{
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+}
+
+bool recoverStream()
+{
+    if (!std::cin)
+    {
+        if (std::cin.eof())
+        {
+            std::exit(1);
+        }
+
+        std::cin.clear();
+        ignoreLine();
+        return true;
+    }
+
+    return false;
+}
+
+int getInteger(const std::string& prompt)
+{
+    while (true)
+    {
+        int x{};
+        std::cout << prompt;
+        std::cin >> x;
+
+        if (recoverStream() || x < 0)
+        {
+            std::cout << "Invalid input. Try again.\n";
+            continue;
+        }
+
+        return x;
+    }
+}
