b2dc0ae747ba97e0d0cead6a86e03e533a2213f3
dozothebozo <dozothebozo@gmail.com>
2026-04-05 14:43:18 -0400

    add(Practice/twinPrime): solution

--- Practice/twinPrime.cpp
+++ Practice/twinPrime.cpp
@@ -5,3 +5,97 @@
 // is a pair of prime numbers that differ by 2. For example, 3 and 5 are twin prime numbers, 5 and 7
 // are twin prime numbers, etc. Print each pair of twin prime numbers in parentheses. Print four
 // pairs per line, in a well aligned table.
+
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// function prototypes
+bool isPrime(int num);
+vector<int> getPrimeInRange(int min, int max);
+void printTwinPrime(vector<int>& primes);
+
+// global declarations of constants
+const int INTERVAL_MIN{1};
+const int INTERVAL_MAX{10000};
+const int ALIGN{4}; // four pairs per line
+
+int main()
+{
+    vector<int> primes{getPrimeInRange(INTERVAL_MIN, INTERVAL_MAX)};
+    printTwinPrime(primes);
+    return 0;
+}
+
+bool isPrime(int num)
+{
+    if (num <= 1)
+    {
+        return false;
+    }
+
+    if (num == 2)
+    {
+        return true;
+    }
+
+    if (num % 2 == 0)
+    {
+        return false;
+    }
+
+    for (int i{3}; i <= sqrt(num); ++i)
+    {
+        if (num % i == 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+vector<int> getPrimeInRange(int min, int max)
+{
+    vector<int> primes{};
+
+    for (int i{min}; i <= max; ++i)
+    {
+        if (isPrime(i))
+        {
+            primes.push_back(i);
+        }
+    }
+
+    return primes;
+}
+
+void printTwinPrime(vector<int>& primes)
+{
+    int count{};
+
+    for (unsigned int i{0}; i < primes.size() - 1; ++i)
+    {
+        if (primes[i + 1] - primes[i] == 2)
+        {
+            cout << '(' << primes[i] << ',' << primes[i + 1] << ')';
+            cout << setw(6) << ' ';
+
+            ++count;
+
+            if (count == ALIGN)
+            {
+                cout << '\n';
+                count = 0;
+            }
+        }
+    }
+
+    if (count != 0)
+    {
+        cout << '\n';
+    }
+}
